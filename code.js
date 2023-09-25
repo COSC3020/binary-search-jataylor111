531c36e4ecb2c2d6dfe47cdbba972ec3284e653f
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-09-25 13:52:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function binarySearch(list, element) {
+    //This is my binary search function, I apologize for taking so long, I'll talk about it more in my Reflection tomorrow
+    let start = 0;
+    let end = list.length - 1;
+
+    while(start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (list[middle] < element) {
+            start = middle + 1;
+        }
+        if (list[middle] > element) {
+            end = middle - 1;
+        }
+        if (middle == element) {
+            return middle;
+        }
+    }
     return -1;
 }
