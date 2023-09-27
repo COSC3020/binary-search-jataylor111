3bbf4ee11b4dfb9885a533492f13cfb276a5d37f
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-09-27 15:15:42 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -5,6 +5,12 @@ eval(fs.readFileSync('code.js')+'');
 
 const testSearch =
     jsc.forall("array nat", function(arr) {
+        //I've added a loop that checks to make sure that array[0] is less than array[1] and so on to the end of the array
+        for(let i = 0; i < arr.length; i++) {
+            let i2 = i + 1;
+            if((arr[i2] - arr[i]) < 0) { return false }
+        }
+        
         if(arr.length > 0) {
             return binarySearch(arr, arr[0]) === 0;
         } else {
