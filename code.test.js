c80eb3c67ba6505c9cffb09dc7e8662a08045e1b
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-09-27 16:58:46 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -5,17 +5,22 @@ eval(fs.readFileSync('code.js')+'');
 
 const testSearch =
     jsc.forall("array nat", function(arr) {
-        //I've added a loop that checks to make sure that array[0] is less than array[1] and so on to the end of the array
-        for(let i = 0; i < arr.length; i++) {
-            let i2 = i + 1;
-            if((arr[i2] - arr[i]) < 0) { return false }
+        //I've replaced that loop with a small insertion sort loop just to ensure it's sorted
+        for(var i = 0; i < arr.length; i++) {
+            var val = arr[i];
+            var j;
+            for(j = i; j > 0 && arr[j-1] > val; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = val;
         }
-        
+
         if(arr.length > 0) {
             return binarySearch(arr, arr[0]) === 0;
         } else {
             return binarySearch(arr, "foo") === -1;
         }
+        
     });
 
 jsc.assert(testSearch);
